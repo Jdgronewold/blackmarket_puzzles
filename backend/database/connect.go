ec6e40cefaa5dbf0f0363646e2bef9ca5bfda92d
Jeff Gronewold <jeff.gronewold@gmail.com>
2022-06-05 07:37:46 -0600

    config

--- backend/database/connect.go
+++ backend/database/connect.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"strconv"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -20,7 +20,7 @@ func ConnectDB() {
 		panic("failed to read config from env")
 	}
 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
